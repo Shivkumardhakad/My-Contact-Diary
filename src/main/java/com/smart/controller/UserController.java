f393f80e1767d51c89fde09a40f11aa03902cec7
Shivkumardhakad <shivkumardhakad9981@gmail.com>
2025-12-30 11:31:01 +0530

    updated UserController

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -15,8 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -160,6 +163,13 @@ String name=principal.getName();
 		return "normal/show_contacts";
 	}
 	
+// delete contact handler
+@DeleteMapping("/contact/{id}")
+public String deleteContact(@PathVariable("id") int id ) {
+	
+	
+	return "";
+}
 	
 	
 }
