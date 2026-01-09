918aa0ab53af10c32720d647f47c6520bd952ec2
Shivkumardhakad <shivkumardhakad9981@gmail.com>
2026-01-10 00:00:13 +0530

    mvc config added

---
+++ src/main/java/com/smart/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.smart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Ye line batati hai ki "/img/" request ko system ke temp folder mein map karna hai
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:" + System.getProperty("java.io.tmpdir") + "/");
+    }
+}
